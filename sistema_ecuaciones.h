fadb4da6c64bbe02589f0f56dbb9e6fcbe7c1546
Michiswww <villacorteg123@gmail.com>
2026-07-12 17:09:29 -0500

    Sistemas de ecuaciones Gauss Jordan

---
+++ sistema_ecuaciones.h
@@ -0,0 +1,6 @@
+void mostrarIncognita(int j, int n);
+void ingresarEcuaciones(float matriz[][11], int n);
+void mostrarSistema(float matriz[][11], int n);
+void gaussJordan(float matriz[][11], int n);
+int analizarSolucion(float matriz[][11], int n);
+void mostrarSolucion(float matriz[][11], int n);
